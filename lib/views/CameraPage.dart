a90864ffd61e609753a19ecc766c09a1630aae5c
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-07-19 09:17:13 -0400

    flitter graphql is dumb

--- lib/views/CameraPage.dart
+++ lib/views/CameraPage.dart
@@ -3,6 +3,11 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'dart:io';
+import 'dart:convert';
+
+import '../services/api.services.dart';
 
 class CameraPage extends StatefulWidget {
   final CameraDescription camera;
@@ -61,20 +66,56 @@ class _CameraPageState extends State<CameraPage> {
 
         onPressed: () async {
 
-          try {
-            await _initalizedControllerFuture;
+          //grab picture in base64
+          getBase64Pic().then((encoded) => {
+            
+            Query(
+              options: QueryOptions(
+                documentNode: gql(GET_BOOK),
+                variables: {
+                  'imgBinary': encoded
+                }
+              ),
 
-            final path = join((await getTemporaryDirectory()).path, '${DateTime.now()}.png' ); 
+              builder: (QueryResult result, {VoidCallback refetch, FetchMore fetchMore}) {
 
-            await _controller.takePicture(path);
+                if (result.hasException) {
+                  // print(result.exception);
+                }
 
-          } catch(e) {
-            print(e);
+                if (result.loading) {
+                }
 
-          }
+                // print('resutl'+result.data.toString());
+
+              }
+            )
+
+          });
+
 
         },
       ),
     );
+  }
+
+  Future<String> getBase64Pic() async {
+
+    try {
+      await _initalizedControllerFuture;
+
+      final path = join((await getTemporaryDirectory()).path, '${DateTime.now()}.png' ); 
+
+      ImageCache().clear();
+      await _controller.takePicture(path);
+
+      List<int> bytes = await File(path).readAsBytes(); 
+      return base64.encode(bytes);
+
+    } catch(e) {
+      print(e);
+
+    }
+
   }
 }
